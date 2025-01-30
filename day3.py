0fb6ccfe5767f551f61b382b6885b787a5d42892
Aknandan Sharma <aknandansharma@gmail.com>
2025-01-31 01:03:00 +0530

    day3

---
+++ day3.py
@@ -0,0 +1,120 @@
+# a = "aknandan"; print(a[7])
+# a = "aknandan"
+# for x in a:
+#     print(x)
+
+
+# a = "aknandan_kumar"
+# print(len(a))
+
+
+# txt = "The best things in life are free";
+# print("best" in txt)
+
+
+
+# txt = "The best things in life are free";
+# if "free" in txt:
+#     print("Yes 'free' is Present")
+
+# txt = "The best things in life are free";
+# print("naknk" not in txt)
+
+
+# txt = "The best things in life are free!"
+# if "bes9t" not in txt:
+#   print("No, 'expensive' is NOT present.")
+
+
+# a = "Hello, World!";
+# print(a[-5:-2])
+
+# a = "  Hello,    World!   "
+# print(a.strip())
+
+# a = "Hello, World";
+# print(a.replace("H", "A"))
+
+# a = "Hello, World, Aknandan, sharma";
+# print(a.split("."))
+
+# a = 26;
+
+# txt = f"The price is {a:.1f} dollars"
+# print(txt)
+
+# txt = f"The price is {0 * 59} dollars"
+# txt = "We are \rthe \t so-called \"Vikings\" from the north."
+
+# print(txt)
+
+# a = "aknandnA";
+# x = a.count()
+# print(coux)
+
+# print(10<120)
+# print(10>20)
+# print(10 == 20)
+# print(10 == 10)
+
+# a = 200
+# b = 33
+
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
+
+
+# print(bool(""))
+# print(bool("15"))
+
+
+# x = "Hello"
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
+
+
+# bool("abc")
+# bool(123)
+# bool(["apple", "cherry", "banana"])
+
+
+# class myclass():
+#   def __len__(self):
+#     return 0
+
+# myobj = myclass()
+# print(bool(myobj))
+
+
+# def myFun():
+#     return True;
+
+# if myFun():
+#     print("Yes")
+# else:
+#     print("No")
+# x = 5;
+# x = x | 3;
+# print(x)
+
+
+# print((16 + 3) - (6 + 3))
+
+# print(100 * 5 + 3)
+# print(5 + 4 - 7 + 3)
+
+"""
+    Tomrrow start the "Python List"
+    
+"""
+
+
+
+
+
+
+
